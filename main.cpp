39d0ff6db612dffdcac781bb2b85277b3559c595
Hammad Ali <chh68815@gmail.com>
2024-04-16 07:23:33 +0500

    main code

--- main.cpp
+++ main.cpp
@@ -5,6 +5,47 @@ using namespace std;
 
 int main()
 {
-    cout << "i am in my calculator project\n";
+    double num1 {};
+    double num2 {};
+    int choice {};
+    char c {};
+    cout << "Welcome to this simple calculator program\n";
+    do{
+        cout << "Following operations are supported: \n";
+        cout << "1. Addition\n";
+        cout << "2. Subtraction\n";
+        cout << "3. Multiplication\n";
+        cout << "4. Division\n";
+        cout << "5. Exit\n";
+        cout << "Enter you choice: ";
+        cin >> choice;
+
+        cout << "Enter first number: ";
+        cin >> num1;
+        cout << "Enter second number: ";
+        cin >> num2;
+        switch (choice)
+        {
+            case 1:
+                cout << "Result: " << num1 + num2 << endl;
+                break;
+            case 2:
+                cout << "Result: " << num1 - num2 << endl;
+                break;
+            case 3:
+                cout << "Result: " << num1 * num2 << endl;
+                break;
+            case 4: 
+                cout << "Result: " << num1 / num2 << endl;
+                break;
+            case 5:
+                exit(0);
+        }
+
+        cout << "Do you want to continue (y or n): ";
+        cin >> c;
+        cout << "\n";
+
+    } while (c != 'n' && c !='N');
     return 0;
 }
